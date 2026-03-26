2fec1c7a97353e3c6ad1475bd6826e73845f2946
Rohitkumar Jadhav <rohitjadhav48@gmail.com>
2026-03-26 14:43:30 +0100

    Increase logo size and add shadow for better visibility

--- src/sections/Navigation.tsx
+++ src/sections/Navigation.tsx
@@ -51,7 +51,7 @@ const Navigation = () => {
             <img 
               src={`${import.meta.env.BASE_URL}logo.png`} 
               alt="Anarion" 
-              className="h-8 w-auto object-contain brightness-0 invert"
+              className="h-14 w-auto object-contain brightness-0 invert drop-shadow-lg"
             />
           </a>
 
